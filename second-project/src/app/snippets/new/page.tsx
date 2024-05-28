33a46b33afe6add4fc3821de08c617cc4e9e7256
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2024-05-28 08:13:05 -0300

    Introducing Server Actions in Next.js

--- second-project/src/app/snippets/new/page.tsx
+++ second-project/src/app/snippets/new/page.tsx
@@ -1,6 +1,23 @@
+import { redirect } from "next/navigation";
+import { db } from "@/db";
+
 export default function SnippetCreatePage() {
+  async function createSnippet(formData: FormData) {
+    "use server";
+    const title = formData.get("title") as string;
+    const code = formData.get("code") as string;
+    const snippet = await db.snippet.create({
+      data: {
+        title: title,
+        code: code,
+      },
+    });
+    console.log(snippet);
+    redirect("/");
+  }
+
   return (
-    <form>
+    <form action={createSnippet}>
       <h3 className="font-bold m-3">Create a Snippet</h3>
       <div className="flex flex-col gap-4">
         <div className="flex gap-4">
@@ -25,7 +42,7 @@ export default function SnippetCreatePage() {
           />
         </div>
         <button type="submit" className="rounded p-2 bg-blue-200">
-            Create
+          Create
         </button>
       </div>
     </form>
